5d299f84068bb877ed4fb8dd09b47ae95286c90f
writeone <chaiprapun@hotmail.com>
2018-11-13 20:44:28 +0700

    - apply tslint

--- src/app/components/exam-chart/exam-chart.component.ts
+++ src/app/components/exam-chart/exam-chart.component.ts
@@ -19,47 +19,47 @@ export class ExamChartComponent implements OnInit {
   private initialLoadChart() {
 
     // Data for Line Chart
-    var data = {
-      labels: ["January", "February", "March", "April", "May"],
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May'],
       datasets: [
         {
-          label: "My First dataset",
-          backgroundColor: "rgba(220,220,220,0.2)",
-          borderColor: "rgba(220,220,220,1)",
-          pointBackgroundColor: "rgba(220,220,220,1)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          label: 'My First dataset',
+          backgroundColor: 'rgba(220,220,220,0.2)',
+          borderColor: 'rgba(220,220,220,1)',
+          pointBackgroundColor: 'rgba(220,220,220,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
           data: [65, 59, 80, 81, 56]
         },
         {
-          label: "My Second dataset",
-          backgroundColor: "rgba(151,187,205,0.2)",
-          borderColor: "rgba(151,187,205,1)",
-          pointBackgroundColor: "rgba(151,187,205,1)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(151,187,205,1)",
+          label: 'My Second dataset',
+          backgroundColor: 'rgba(151,187,205,0.2)',
+          borderColor: 'rgba(151,187,205,1)',
+          pointBackgroundColor: 'rgba(151,187,205,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(151,187,205,1)',
           data: [28, 48, 40, 19, 86]
         }
       ]
     };
 
     // Data for Bar Chart
-    var bdata = {
-      labels: ["January", "February", "March", "April", "May"],
+    const bdata = {
+      labels: ['January', 'February', 'March', 'April', 'May'],
       datasets: [
         {
-          label: "My First dataset",
-          backgroundColor: "rgba(220,220,220,0.2)",
-          borderColor: "rgba(220,220,220,1)",
+          label: 'My First dataset',
+          backgroundColor: 'rgba(220,220,220,0.2)',
+          borderColor: 'rgba(220,220,220,1)',
           borderWidth: 2,
           data: [65, 59, 80, 81, 56]
         },
         {
-          label: "My Second dataset",
-          backgroundColor: "rgba(151,187,205,0.2)",
-          borderColor: "rgba(151,187,205,1)",
+          label: 'My Second dataset',
+          backgroundColor: 'rgba(151,187,205,0.2)',
+          borderColor: 'rgba(151,187,205,1)',
           borderWidth: 2,
           data: [28, 48, 40, 19, 86]
         }
@@ -67,56 +67,56 @@ export class ExamChartComponent implements OnInit {
     };
 
     // Data for Horizontal Bar Chart
-    var hbdata = {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    const hbdata = {
+      labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
       datasets: [
         {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          label: 'Population (millions)',
+          backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+          data: [2478, 5267, 734, 784, 433]
         }
       ]
     };
 
     // Data for Grouped Bar Chart
-    var gbdata = {
-      labels: ["1900", "1950", "1999", "2050"],
+    const gbdata = {
+      labels: ['1900', '1950', '1999', '2050'],
       datasets: [
         {
-          label: "Africa",
-          backgroundColor: "#3e95cd",
-          data: [133,221,783,2478]
+          label: 'Africa',
+          backgroundColor: '#3e95cd',
+          data: [133, 221, 783, 2478]
         }, {
-          label: "Europe",
-          backgroundColor: "#8e5ea2",
-          data: [408,547,675,734]
+          label: 'Europe',
+          backgroundColor: '#8e5ea2',
+          data: [408, 547, 675, 734]
         }
       ]
     };
 
     // Data for Radar Chart
-    var rdata = {
-      labels: ["January", "February", "March", "April", "May"],
+    const rdata = {
+      labels: ['January', 'February', 'March', 'April', 'May'],
       datasets: [
         {
-          label: "My First dataset",
-          backgroundColor: "rgba(220,220,220,0.2)",
-          borderColor: "rgba(220,220,220,1)",
-          pointBackgroundColor: "rgba(220,220,220,1)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          label: 'My First dataset',
+          backgroundColor: 'rgba(220,220,220,0.2)',
+          borderColor: 'rgba(220,220,220,1)',
+          pointBackgroundColor: 'rgba(220,220,220,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
           borderWidth: 2,
           data: [65, 59, 80, 81, 56]
         },
         {
-          label: "My Second dataset",
-          backgroundColor: "rgba(151,187,205,0.2)",
-          borderColor: "rgba(151,187,205,1)",
-          pointBackgroundColor: "rgba(151,187,205,1)",
-          pointBorderColor: "#fff",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(151,187,205,1)",
+          label: 'My Second dataset',
+          backgroundColor: 'rgba(151,187,205,0.2)',
+          borderColor: 'rgba(151,187,205,1)',
+          pointBackgroundColor: 'rgba(151,187,205,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(151,187,205,1)',
           borderWidth: 2,
           data: [28, 48, 40, 19, 86]
         }
@@ -124,51 +124,51 @@ export class ExamChartComponent implements OnInit {
     };
 
     // Data for Pie Chart
-    var pdata = {
-      labels: ["Red", "In-Green", "Yellow"],
+    const pdata = {
+      labels: ['Red', 'In-Green', 'Yellow'],
       datasets: [
         {
           data: [300, 50, 100],
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
+          backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C'],
+          hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870']
         }
       ]
-    }
+    };
 
     // Data for Bubble Chart
-    var bbdata = {
+    const bbdata = {
       datasets: [
         {
-          label: ["China"],
-          backgroundColor: "rgba(255,221,50,0.2)",
-          borderColor: "rgba(255,221,50,1)",
+          label: ['China'],
+          backgroundColor: 'rgba(255,221,50,0.2)',
+          borderColor: 'rgba(255,221,50,1)',
           data: [{
             x: 21269017,
             y: 5.245,
             r: 120
           }]
         }, {
-          label: ["Denmark"],
-          backgroundColor: "rgba(60,186,159,0.2)",
-          borderColor: "rgba(60,186,159,1)",
+          label: ['Denmark'],
+          backgroundColor: 'rgba(60,186,159,0.2)',
+          borderColor: 'rgba(60,186,159,1)',
           data: [{
             x: 258702,
             y: 7.526,
             r: 10
           }]
         }, {
-          label: ["Germany"],
-          backgroundColor: "rgba(0,0,0,0.2)",
-          borderColor: "#000",
+          label: ['Germany'],
+          backgroundColor: 'rgba(0,0,0,0.2)',
+          borderColor: '#000',
           data: [{
             x: 3979083,
             y: 6.994,
             r: 7.17
           }]
         }, {
-          label: ["Japan"],
-          backgroundColor: "rgba(193,46,12,0.2)",
-          borderColor: "rgba(193,46,12,1)",
+          label: ['Japan'],
+          backgroundColor: 'rgba(193,46,12,0.2)',
+          borderColor: 'rgba(193,46,12,1)',
           data: [{
             x: 4931877,
             y: 5.921,
@@ -176,73 +176,72 @@ export class ExamChartComponent implements OnInit {
           }]
         }
       ]
-    }
+    };
 
-    
-    var ctxl = $("#lineChartDemo").get(0).getContext("2d");
-    var lineChart = new Chart(ctxl, {
-      type: "line",
+    const ctxl = $('#lineChartDemo').get(0).getContext('2d');
+    const lineChart = new Chart(ctxl, {
+      type: 'line',
       data: data,
     });
 
-    var ctxb = $("#barChartDemo").get(0).getContext("2d");
-    var barChart = new Chart(ctxb, {
-      type: "bar",
+    const ctxb = $('#barChartDemo').get(0).getContext('2d');
+    const barChart = new Chart(ctxb, {
+      type: 'bar',
       data: bdata,
     });
 
-    var ctxr = $("#radarChartDemo").get(0).getContext("2d");
-    var radarChart = new Chart(ctxr, {
-      type: "radar",
+    const ctxr = $('#radarChartDemo').get(0).getContext('2d');
+    const radarChart = new Chart(ctxr, {
+      type: 'radar',
       data: rdata,
     });
 
-    var ctxpo = $("#polarChartDemo").get(0).getContext("2d");
-    var polarChart = new Chart(ctxpo, {
-      type: "polarArea",
+    const ctxpo = $('#polarChartDemo').get(0).getContext('2d');
+    const polarChart = new Chart(ctxpo, {
+      type: 'polarArea',
       data: pdata
     });
 
-    var ctxp = $("#pieChartDemo").get(0).getContext("2d");
-    var pieChart = new Chart(ctxp, {
-      type: "pie",
+    const ctxp = $('#pieChartDemo').get(0).getContext('2d');
+    const pieChart = new Chart(ctxp, {
+      type: 'pie',
       data: pdata
     });
 
-    var ctxd = $("#doughnutChartDemo").get(0).getContext("2d");
-    var doughnutChart = new Chart(ctxd, {
-      type: "doughnut",
+    const ctxd = $('#doughnutChartDemo').get(0).getContext('2d');
+    const doughnutChart = new Chart(ctxd, {
+      type: 'doughnut',
       data: pdata
     });
 
-    var ctxbb = $("#bubbleChartDemo").get(0).getContext("2d");
-    var bubbleChart = new Chart(ctxbb, {
-      type: "bubble",
+    const ctxbb = $('#bubbleChartDemo').get(0).getContext('2d');
+    const bubbleChart = new Chart(ctxbb, {
+      type: 'bubble',
       data: bbdata,
       options: {
         title: {
           display: true,
           text: 'Predicted world population (millions) in 2050'
         }, scales: {
-          yAxes: [{ 
+          yAxes: [ {
             scaleLabel: {
               display: true,
-              labelString: "Happiness"
+              labelString: 'Happiness'
             }
           }],
-          xAxes: [{ 
+          xAxes: [ {
             scaleLabel: {
               display: true,
-              labelString: "GDP (PPP)"
+              labelString: 'GDP (PPP)'
             }
           }]
         }
       }
     });
 
-    var ctxhb = $("#HBarChartDemo").get(0).getContext("2d");
-    var HBarChart = new Chart(ctxhb, {
-      type: "horizontalBar",
+    const ctxhb = $('#HBarChartDemo').get(0).getContext('2d');
+    const HBarChart = new Chart(ctxhb, {
+      type: 'horizontalBar',
       data: hbdata,
       options: {
         legend: { display: false },
@@ -253,9 +252,9 @@ export class ExamChartComponent implements OnInit {
       }
     });
 
-    var ctxgb = $("#GBarChartDemo").get(0).getContext("2d");
-    var GBarChart = new Chart(ctxgb, {
-      type: "bar",
+    const ctxgb = $('#GBarChartDemo').get(0).getContext('2d');
+    const GBarChart = new Chart(ctxgb, {
+      type: 'bar',
       data: gbdata,
       options: {
         title: {
