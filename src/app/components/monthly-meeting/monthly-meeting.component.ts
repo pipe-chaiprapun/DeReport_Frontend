a00409088d5b31578fa053c191c083f2dbc3d0ce
writeone <chaiprapun@hotmail.com>
2018-11-20 16:45:22 +0700

    - update monthly meeting increase decrease - add filter for sale-info

--- src/app/components/monthly-meeting/monthly-meeting.component.ts
+++ src/app/components/monthly-meeting/monthly-meeting.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AppserverService } from '../../services/appserver.service';
+import { noUndefined } from '@angular/compiler/src/util';
 declare const App;
 declare const $;
+const monthNames = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษพาคม", "มิถุนายน",
+  "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+];
 @Component({
   selector: 'app-monthly-meeting',
   templateUrl: './monthly-meeting.component.html',
@@ -24,12 +28,17 @@ export class MonthlyMeetingComponent implements OnInit {
   private nCustPDOAMT: string;
   private pdoAmtPercent: string;
   private branches = [];
+  private branchInput: HTMLInputElement;
+  private branchIndex: number = 0;
+  private branchNum: number;
+  private month: string;
   constructor(private resAPI: AppserverService) { }
 
   ngOnInit() {
     App.initLoadJquery();
     this.initialLoadChart();
-    $("input[type='number']").inputSpinner();
+    let today = new Date(Date.now());
+    this.month = monthNames[today.getMonth() - 1];
   }
   private devideMillion(data: number) {
     return (data / 1000000).toFixed(2);
@@ -37,6 +46,9 @@ export class MonthlyMeetingComponent implements OnInit {
   private initialLoadChart() {
     this.resAPI.getMonthlyMeeting().subscribe(result => {
       this.branches = result.data;
+      this.branchNum = this.branches.length;
+      this.branchInput = document.getElementById("branchId") as HTMLInputElement;
+      this.branchInput.value = this.branches[0].BRH_ID;
       this.mgrsName = result.data[0].MGRS_NAME;
       this.tarAmt = this.devideMillion(result.data[0].TAR_AMT);
       this.saleAmt = this.devideMillion(result.data[0].SALE_AMT);
@@ -52,30 +64,56 @@ export class MonthlyMeetingComponent implements OnInit {
       this.pdoAmt = this.devideMillion(result.data[0].PDO_AMT);
       this.oCustPDOAMT = this.devideMillion(result.data[0].OCUST_PDO_AMT);
       this.nCustPDOAMT = this.devideMillion(result.data[0].NCUST_PDO_AMT);
-      this.pdoAmtPercent = ((result.data[0].PDO_AMT/result.data[0].FREMAIN_AMT)*100).toFixed(2);
+      this.pdoAmtPercent = ((result.data[0].PDO_AMT / result.data[0].FREMAIN_AMT) * 100).toFixed(2);
     })
   }
   private newRenderChart(brh) {
+    this.branchIndex = this.branches.findIndex(element => element.BRH_ID == brh);
     let branch = this.branches.filter(element => element.BRH_ID == brh)[0];
-    console.log(branch);
-    this.mgrsName = branch.MGRS_NAME;
-    this.tarAmt = this.devideMillion(branch.TAR_AMT);
-    this.saleAmt = this.devideMillion(branch.SALE_AMT);
-    // this.diffTarAmt = devideMillion(result.data[0].DIF_TAR_AMT);
-    this.diffTarAmt = this.devideMillion(branch.SALE_AMT - branch.TAR_AMT);
-    this.numDiffTarAmt = branch.SALE_AMT - branch.TAR_AMT;
-    this.payAmt = this.devideMillion(branch.PAY_AMT);
-    this.accTarAmt = this.devideMillion(branch.ACC_TAR_AMT);
-    this.accSaleAmt = this.devideMillion(branch.ACC_SALE_AMT);
-    this.summary = this.devideMillion(branch.ACC_SALE_AMT - branch.ACC_TAR_AMT);
-    this.numSummary = branch.ACC_SALE_AMT - branch.ACC_TAR_AMT;
-    this.losPdoAmt = this.devideMillion(branch.LOS_PDO_AMT);
-    this.pdoAmt = this.devideMillion(branch.PDO_AMT);
-    this.oCustPDOAMT = this.devideMillion(branch.OCUST_PDO_AMT);
-    this.nCustPDOAMT = this.devideMillion(branch.NCUST_PDO_AMT);
-    this.pdoAmtPercent = ((branch.PDO_AMT/branch.FREMAIN_AMT)*100).toFixed(2);
+    if (noUndefined(branch)) {
+      this.branchInput.value = branch.BRH_ID;
+      this.mgrsName = branch.MGRS_NAME;
+      this.tarAmt = this.devideMillion(branch.TAR_AMT);
+      this.saleAmt = this.devideMillion(branch.SALE_AMT);
+      // this.diffTarAmt = devideMillion(result.data[0].DIF_TAR_AMT);
+      this.diffTarAmt = this.devideMillion(branch.SALE_AMT - branch.TAR_AMT);
+      this.numDiffTarAmt = branch.SALE_AMT - branch.TAR_AMT;
+      this.payAmt = this.devideMillion(branch.PAY_AMT);
+      this.accTarAmt = this.devideMillion(branch.ACC_TAR_AMT);
+      this.accSaleAmt = this.devideMillion(branch.ACC_SALE_AMT);
+      this.summary = this.devideMillion(branch.ACC_SALE_AMT - branch.ACC_TAR_AMT);
+      this.numSummary = branch.ACC_SALE_AMT - branch.ACC_TAR_AMT;
+      this.losPdoAmt = this.devideMillion(branch.LOS_PDO_AMT);
+      this.pdoAmt = this.devideMillion(branch.PDO_AMT);
+      this.oCustPDOAMT = this.devideMillion(branch.OCUST_PDO_AMT);
+      this.nCustPDOAMT = this.devideMillion(branch.NCUST_PDO_AMT);
+      this.pdoAmtPercent = ((branch.PDO_AMT / branch.FREMAIN_AMT) * 100).toFixed(2);
+    }
   }
   onChange(deviceValue) {
     this.newRenderChart(deviceValue);
+  }
+  increase(data) {
+    this.nextItem()
+    // var inputBranch = document.getElementById("branchId") as HTMLInputElement;
+    // let branchId = parseInt(inputBranch.value);
+  }
+  selectBranch(data) {
+    this.newRenderChart(data.value);
+  }
+  decrease(data) {
+    this.prevItem()
+  }
+  nextItem() {
+    this.branchIndex++; // increase i by one
+    this.branchIndex = this.branchIndex % this.branchNum; // if we've gone too high, start from `0` again
+    this.newRenderChart(this.branches[this.branchIndex].BRH_ID);
+  }
+  prevItem() {
+    if (this.branchIndex === 0) { // i would become 0
+      this.branchIndex = this.branchNum; // so put it at the other end of the array
+    }
+    this.branchIndex--; // decrease by one
+    this.newRenderChart(this.branches[this.branchIndex].BRH_ID);
   }
 }
--- src/app/components/sale-info/sale-info.component.ts
+++ src/app/components/sale-info/sale-info.component.ts
@@ -15,8 +15,11 @@ export class SaleInfoComponent implements OnInit {
   private targetAmt = [];
   private saleAmt = [];
   private payAmt = [];
+  private diffTarAmt = [];
   private pdoAmt = [];
-
+  private allData = [];
+  private current;
+  private barChart;
   constructor(private resAPI: AppserverService) { }
 
   ngOnInit() {
@@ -26,15 +29,69 @@ export class SaleInfoComponent implements OnInit {
   private devideMillion(data: number) {
     return (data / 1000000).toFixed(2);
   }
+  onChange(data, select) {
+    //console.log(data);
+    let branch = data.target.innerText;
+    let checked = data.target.childNodes[0].checked;
+    let index = this.allData.findIndex(element => element.BRH_ID == data.target.innerText)
+    console.log(checked);
+    this.newRenderChart(branch, checked, index);
+  }
+  private newRenderChart(brh, display, index) {
+    this.current = this.allData[index];
+    if(display == false){
+      this.labels = this.labels.filter(name => name != brh);
+      this.targetAmt = this.targetAmt.filter(tar => tar != this.current.TAR_AMT);
+      this.saleAmt = this.saleAmt.filter(sale => sale != this.current.SALE_AMT);
+      this.payAmt = this.payAmt.filter(pay => pay != this.current.PAY_AMT);
+      this.pdoAmt = this.pdoAmt.filter(pdo => pdo != this.current.PDO_AMT);
+      this.diffTarAmt = this.diffTarAmt.filter(diff => diff != this.current.DIF_TAR_AMT);
+    }
+    const bdata = {
+      labels: this.labels,
+      datasets: [
+        {
+          label: 'ยอดเป้าหมาย',
+          backgroundColor: constant.tarAmt.background,
+          borderColor: constant.tarAmt.border,
+          borderWidth: 2,
+          data: this.targetAmt 
+        },
+        {
+          label: 'ยอดขาย',
+          backgroundColor: constant.saleAmt.background,
+          borderColor: constant.saleAmt.border,
+          borderWidth: 2,
+          data: this.saleAmt 
+        },
+        {
+          label: 'ขาด/เกิน ยอด',
+          backgroundColor: constant.payAmt.background,
+          borderColor: constant.payAmt.border,
+          borderWidth: 2,
+          data: this.diffTarAmt 
+        },
+        {
+          label: 'PDO',
+          backgroundColor: constant.pdoAmt.background,
+          borderColor: constant.pdoAmt.border,
+          borderWidth: 2,
+          data: this.pdoAmt 
+        }
+      ]
+    };
+    const ctxb = $('#barChartDemo').get(0).getContext('2d');
+    this.barChart.destroy();
+    this.barChart = new Chart(ctxb, {
+      type: 'bar',
+      data: bdata,
+    });
+  }
   private initialLoadChart() {
     this.resAPI.getSaleInfo().subscribe(result => {
       console.log('---------- get sale info --------------------')
       console.log(result.data);
-      // var labels = [];
-      // var targetAmt = [];
-      // var saleAmt = [];
-      // var payAmt = [];
-      // var pdoAmt = [];
+      this.allData = result.data;
       result.data.forEach(element => {
         this.labels.push(element.BRH_ID);
       });
@@ -50,6 +107,10 @@ export class SaleInfoComponent implements OnInit {
       result.data.forEach(element => {
         this.pdoAmt.push(element.PDO_AMT);
       })
+      result.data.forEach(element => {
+        this.diffTarAmt.push(element.DIF_TAR_AMT);
+      });
+
       // ['#F7464A', '#46BFBD', '#FDB45C']
       const bdata = {
         labels: this.labels, //['สาขา02', 'สาขา03', 'สาขา04', 'สาขา05', 'สาขา06', 'สาขา07', 'สาขา08', 'สาขา09', 'สาขา10', 'สาขา11', 'สาขา12', 'สาขา'],
@@ -69,11 +130,11 @@ export class SaleInfoComponent implements OnInit {
             data: this.saleAmt //[60, 48, 85, 75, 60, 84, 56, 34, 25, 56, 76, 56]
           },
           {
-            label: 'ขาดยอด/เกินยอด',
+            label: 'ขาด/เกิน ยอด',
             backgroundColor: constant.payAmt.background,
             borderColor: constant.payAmt.border,
             borderWidth: 2,
-            data: this.payAmt //[-10, 50, 80, 50, -20, 75, 57, 34, 25, 67, 45, 67]
+            data: this.diffTarAmt //[-10, 50, 80, 50, -20, 75, 57, 34, 25, 67, 45, 67]
           },
           {
             label: 'PDO',
@@ -85,10 +146,12 @@ export class SaleInfoComponent implements OnInit {
         ]
       };
       const ctxb = $('#barChartDemo').get(0).getContext('2d');
-      const barChart = new Chart(ctxb, {
+      this.barChart = new Chart(ctxb, {
         type: 'bar',
         data: bdata,
       });
+      let b = document.getElementById("selectBranch");
+      console.log(b);
     })
   }
 }
