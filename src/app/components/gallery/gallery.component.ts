62a56deab0ad430a3d4ccb07159ecaeea69fe693
jenwit <jenwit07@hotmail.com>
2018-12-15 21:06:46 +0700

    gallert manage added

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AppserverService } from '../../services/appserver.service';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { GalleryService } from '../../services/gallery.service';
+import { Http } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'hammerjs';
 declare const App;
 declare const $;
 @Component({
@@ -8,11 +13,46 @@ declare const $;
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  public url;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  pmdayOption: NgxGalleryOptions[];
+  pmdayImage: NgxGalleryImage[];
+  footballOption: NgxGalleryOptions[];
+  footballImage: NgxGalleryImage[];
+  sportOption: NgxGalleryOptions[];
+  sportImage: NgxGalleryImage[];
+  misOption: NgxGalleryOptions[];
+  misImage: NgxGalleryImage[];
 
-  constructor(private resAPI: AppserverService) { }
+  constructor(private http: HttpClient, private __appserverservice: AppserverService, private __galleryservice: GalleryService, ) { 
+     this.url = this.__appserverservice.baseUrl; 
+  }
 
   ngOnInit() {
     App.initLoadJquery();
+    this.gallery();
+  }
+
+  public gallery() {
+    //Main
+    this.galleryOptions = this.__galleryservice.mainOptions;
+    this.galleryImages = this.__galleryservice.mainImages;
+
+    //PMDay
+    this.pmdayOption = this.__galleryservice.bdpmOptions;
+    this.pmdayImage = this.__galleryservice.bdpmImages;
+
+    //แข่งขันฟุตบอลเสียงตามสาย
+    this.footballOption = this.__galleryservice.footballOptions;
+    this.footballImage = this.__galleryservice.footballImages;
+
+    //แข่งขันฟุตบอลเสียงตามสาย
+    this.sportOption = this.__galleryservice.spOptions;
+    this.sportImage = this.__galleryservice.spImages;
+
+    this.misOption = this.__galleryservice.abcOption;
+    this.misImage = this.__galleryservice.abcImage;
   }
 
 }
